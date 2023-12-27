2362e7e1769dca298a80104bea2f1cf2caaac058
Rowland Oka <rowlandoka@gmail.com>
2023-12-27 13:50:06 +0100

    setup controllers and service folder for the api

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 const express = require('express');
 
-// ADD
-const v1Router = require('./v1/routes');
+const v1WorkoutRouter = require('./v1/routes/workoutRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// //for testing purpose
-// app.get('/', (req, res) => {
-// 	res.send("<h1>It's Working!</h1>");
-// });
-
-app.use('/api/v1', v1Router);
+app.use('/api/v1/workouts', v1WorkoutRouter);
 
 app.listen(PORT, () => {
 	console.log(`API is listening on port ${PORT}`);
