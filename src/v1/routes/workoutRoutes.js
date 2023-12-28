02135878f1eabb9101c27f6c7b3301d47a379880
Rowland Oka <rowlandoka@gmail.com>
2023-12-28 07:41:50 +0100

    added routes for all records

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const workoutController = require('../../controllers/workoutController');
 
+const recordController = require('../../controllers/recordController');
+
 const router = express.Router();
 
 router.get('/', workoutController.getAllWorkouts);
 
 router.get('/:workoutId', workoutController.getOneWorkout);
 
+router.get('/:workoutId/records', recordController.getRecordForWorkout);
+
 router.post('/', workoutController.createNewWorkout);
 
-router.patch('/:workoutIt', workoutController.updateOneWorkout);
+router.patch('/:workoutId', workoutController.updateOneWorkout);
 
 router.delete('/:workoutId', workoutController.deleteOneWorkout);
 
